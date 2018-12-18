5dda0927b65741a7f9b35fde4024b9e9269e8c24
manromero <migu621@gmail.com>
2018-12-18 20:55:56 +0100

    scrollable minEvent

--- b-etsii/src/components/MinEventGroup.tsx
+++ b-etsii/src/components/MinEventGroup.tsx
@@ -13,36 +13,49 @@ class MinEventGroup extends React.Component<{}, {showLRIcons: boolean}> {
         this.state = {
             showLRIcons: false
         };
+        this.scroll = this.scroll.bind(this);
+    }
+
+    /**
+     * Realiza scroll sobre el contenido
+     * @param num
+     */
+    public scroll(num: number): void {
+        const elemento = document.getElementById('minEventGroupContent');
+        const currentScroll = elemento!.scrollLeft;
+        elemento!.scrollTo(currentScroll + num, 0);
     }
 
     public render() {
         return (
             <div className="minEventGroupWrapper" onMouseOut={() => this.setState({showLRIcons: false})} onMouseOver={() => this.setState({showLRIcons: true})}>
                 <div className="wrapperIcon">
-                    <FontAwesomeIcon size="5x" className={this.state.showLRIcons ? 'cPointer' : 'cPointer hidden'} icon="chevron-left" />
-                </div>
-                <div className="d-flex flex-row bd-highlight mb-3 align-items-center clearfix">
-                    <div className="p-2 bd-highlight">
-                        <MinEvent />
+                    <div onClick={() => this.scroll(-400)} className="float-left">
+                        <FontAwesomeIcon size="5x"  className={'cPointer' + (this.state.showLRIcons ? '' : ' hidden')} icon="chevron-left" />
                     </div>
-                    <div className="p-2 bd-highlight">
-                        <MinEvent />
-                    </div>
-                    <div className="p-2 bd-highlight">
-                        <MinEvent />
-                    </div>
-                    <div className="p-2 bd-highlight">
-                        <MinEvent />
-                    </div>
-                    <div className="p-2 bd-highlight">
-                        <MinEvent />
-                    </div>
-                    <div className="p-2 bd-highlight">
-                        <MinEvent />
+                    <div onClick={() => this.scroll(400)} className="float-right">
+                        <FontAwesomeIcon size="5x" className={'cPointer' + (this.state.showLRIcons ? '' : ' hidden')} icon="chevron-right" />
                     </div>
                 </div>
-                <div className="wrapperIcon">
-                    <FontAwesomeIcon size="5x" className={this.state.showLRIcons ? 'cPointer float-right' : 'cPointer float-right hidden'} icon="chevron-right" />
+                {/* d-flex flex-row bd-highlight mb-3 align-items-center */}
+                <div id="minEventGroupContent" className="clearfix">
+                    <div className="w-30 p-2 bd-highlight">
+                        <MinEvent />
+                    </div>
+                    <div className="w-30 p-2 bd-highlight">
+                        <MinEvent />
+                    </div>
+                    <div className="w-30 p-2 bd-highlight">
+                        <MinEvent />
+                    </div>
+
+                    <div className="w-30 p-2 bd-highlight">
+                        <MinEvent />
+                    </div>
+
+                    <div className="w-30 p-2 bd-highlight">
+                        <MinEvent />
+                    </div>
                 </div>
             </div>
         );
